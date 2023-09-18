85db37e6ca2ac17c97a07e3ce4b7f0a8b679f845
ashok-choudhary-0 <ashok.kumar@innotechteam.in>
2023-09-18 10:34:40 +0530

    improve code

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,12 @@
 const userModel = require("../models/user");
 const roles = require("../models/roles")
 const bcrypt = require('bcrypt');
+import { getRole } from "./rolesController";
 const userRegister = async (req, res) => {
   try {
     const { username, password, confirmPassword, email, firstName, lastName, roleId } = req.body
-
-    if (password == confirmPassword) {
-      const searchIdInRoles = await roles.findOne({ where: { id: roleId } })
+    if (password === confirmPassword) {
+      const searchIdInRoles = await getRole(roleId)
       if (searchIdInRoles) {
         const hashedPassword = bcrypt.hashSync(password, 10);
         const addedUser = await userModel.create({ email, firstName, lastName, roleId, username, password: hashedPassword });
