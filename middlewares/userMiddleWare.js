43af8b16877c746aa6bf9dda30d28f05e225764a
ashok-choudhary-0 <ashok.kumar@innotechteam.in>
2023-09-19 19:11:34 +0530

    improve code

--- middlewares/userMiddleWare.js
+++ middlewares/userMiddleWare.js
@@ -1,25 +1,10 @@
-const accessTokenModel = require("../models/access_token");
-// const validateToken = async (req, res, next) => {
-//   const { id } = req.headers
-//   if (id) {
-//     next();
-//   } else {
-//     res.status(500).send({ message: "Please enter id" })
-//   }
-// }
-
-
-
 const validateToken = async (req, res, next) => {
-  const {token} = req.headers
-  const userData = await accessTokenModel.findOne({where:{access_token:token}})
-  const expiryTime = userData.expiry
-  const currentTime = new Date();
-  if (currentTime <= expiryTime) {
+  const { id } = req.headers
+  if (id) {
     next();
   } else {
-    res.status(500).send({ message: "invalid token" })
+    res.status(500).send({ message: "Please enter id" })
   }
-  
 }
+
 module.exports = { validateToken }
--- router/userRouter.js
+++ router/userRouter.js
@@ -4,7 +4,7 @@ const userController = require("../controllers/userController")
 const { validateToken } = require("../middlewares/userMiddleWare")
 
 userRouter.post("/register", userController.userRegister)
-userRouter.post("/login", userController.login)
+userRouter.post("/login", validateToken, userController.login)
 userRouter.get("/get", validateToken, userController.getUserData)
 userRouter.delete("/delete", validateToken, userController.deleteUserData)
 userRouter.get("/list/:page", userController.limitUsersData)
