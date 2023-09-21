f5f902eb69e40924885b405f500bb8dd85b787be
ashok-choudhary-0 <ashok.kumar@innotechteam.in>
2023-09-21 10:41:28 +0530

    add some conditions in validatetoken

--- middlewares/userMiddleWare.js
+++ middlewares/userMiddleWare.js
@@ -11,13 +11,21 @@ const accessTokenModel = require("../models/access_token");
 
 const validateToken = async (req, res, next) => {
   const { token } = req.headers
+  if (!token) {
+    res.status(404).send({ message: "token not found" })
+    return;
+  }
   const userData = await accessTokenModel.findOne({ where: { access_token: token } })
-  const expiryTime = userData.expiry
-  const currentTime = new Date();
-  if (currentTime < expiryTime) {
-    next();
+  if (userData) {
+    const expiryTime = userData.expiry
+    const currentTime = new Date();
+    if (currentTime < expiryTime) {
+      next();
+    } else {
+      res.status(500).send({ message: "invalid token" })
+    }
   } else {
-    res.status(500).send({ message: "invalid token" })
+    res.status(500).send({ message: "token not match any user to database" })
   }
 }
 
