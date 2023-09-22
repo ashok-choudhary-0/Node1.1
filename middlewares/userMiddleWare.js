2e7aabd61a292b31a4476154760f94d6d48250fd
ashok-choudhary-0 <ashok.kumar@innotechteam.in>
2023-09-22 16:06:15 +0530

    write all logic in middleware fun

--- middlewares/userMiddleWare.js
+++ middlewares/userMiddleWare.js
@@ -1,6 +1,9 @@
 const accessTokenModel = require("../models/access_token");
-const { initializingPassport } = require("./passportConfig")
-
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require("../models/user");
+const bcrypt = require('bcrypt');
+// const passport = require('passport');
 // initializingPassport(passport)
 
 // const validateToken = async (req, res, next) => {
@@ -34,4 +37,38 @@ const validateToken = async (req, res, next) => {
   }
 }
 
-module.exports = { validateToken }
+const loginAuthentication = (req, res, next) => {
+  passport.use(new LocalStrategy(
+    async (username, password, done) => {
+      try {
+        const user = await User.findOne({ where: { username } });
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      } catch (error) {
+        return done(error);
+      }
+
+    }
+  ));
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findOne({ where: { id } });
+      done(null, user);
+    } catch (err) {
+      done(err, false);
+    }
+  });
+  passport.authenticate('local', { failureMessage: true })
+  next()
+}
+
+module.exports = { validateToken, loginAuthentication }
--- router/userRouter.js
+++ router/userRouter.js
@@ -2,11 +2,10 @@ const express = require("express")
 const userRouter = express.Router();
 const userController = require("../controllers/userController")
 const { validateToken } = require("../middlewares/userMiddleWare")
-const passport = require('passport');
-const { initializingPassport } = require("../middlewares/passportConfig")
+const { loginAuthentication } = require("../middlewares/userMiddleWare")
 
 userRouter.post("/register", userController.userRegister)
-userRouter.post("/login", initializingPassport, passport.authenticate('local', { failureMessage: true }), userController.login)
+userRouter.post("/login", loginAuthentication, userController.login)
 userRouter.get("/get", validateToken, userController.getUserData)
 userRouter.delete("/delete", validateToken, userController.deleteUserData)
 userRouter.get("/list/:page", userController.limitUsersData)
